d7ff912a518c72f1fc6e5d5564b0152bd222c939
kikulwe <andrewkikux82@gmail.com>
2023-07-26 15:20:14 +0300

    templates and views

--- authenticate/views.py
+++ authenticate/views.py
@@ -1,21 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.views.decorators.csrf import csrf_protect
 # Create your views here.
 
+@csrf_protect
 def home(request):
+    if request.method == "POST":
+        #username = request.POST.get('username')
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        myuser = User.objects.create_user(username, email, pass1 )
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+        messages.success(request, "your account has been successfully created.")
+
+        return redirect('signin')
+
+
+
     return render(request, "authentication/home.html")
 
 def signup(request):
+
     return render(request, "authentication/signup.html")
 
 def signin(request):
-    return render(request, "authentication/signin") 
+    return render(request, "authentication/signin.html") 
 
 def signout():
     pass
 
-def signup(request):
-    return render(request, "authentication/signup.html")
-
-def signin(request):
-    return render(request, "authentication/signin") 
