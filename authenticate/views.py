43f4f3aa3f8f8d7cded5c3edcf16c893dc8b2d4e
kikulwe <kikulwe82@gmail.com>
2023-07-28 16:29:31 +0300

    views for signin/login

--- authenticate/views.py
+++ authenticate/views.py
@@ -1,12 +1,19 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.views.decorators.csrf import csrf_protect
+
+import authenticate
 # Create your views here.
 
-@csrf_protect
+
 def home(request):
+    return render(request, "authentication/home.html")
+
+@csrf_protect
+def signup(request):
     if request.method == "POST":
         #username = request.POST.get('username')
         username = request.POST['username']
@@ -25,15 +32,24 @@ def home(request):
 
         return redirect('signin')
 
-
-
-    return render(request, "authentication/home.html")
-
-def signup(request):
-
     return render(request, "authentication/signup.html")
 
 def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+        user = authenticate(username=username, password=pass1)
+
+        if user is not None:
+            login(request,user)
+            fname = user.first_name
+            return render(request, "authentication/index.html",{'fname': fname})
+
+        else:
+            messages.error(request, "Bad Credentials")    
+            return redirect('home')
+
+
     return render(request, "authentication/signin.html") 
 
 def signout():
